4d92b5c6618b06b2af8db841764c1a1f6753ac08
Nic <nicjorgi@gmail.com>
2018-04-04 11:28:52 -0600

    Vue/Front-End Setup

--- noseyjaguar.sql
+++ noseyjaguar.sql
@@ -1,5 +1,5 @@
 -- --CREATE TABLE IN DB
--- CREATE TABLE burgers (
+-- CREATE TABLE noseyjaguar.burgers (
 --   id int NOT NULL AUTO_INCREMENT,
 --   name VARCHAR(255) NOT NULL,
 --   description VARCHAR(255),
@@ -9,34 +9,34 @@
 -- );
 
 -- -- ADD ITEM TO DB_TABLE
--- INSERT INTO burgers (
+-- INSERT INTO noseyjaguar.burgers (
 --   name,
 --   description,
 --   price,
 --   kcal
 -- ) VALUES (
---   "The BBQ Burger",
---   "Tasty meat with BBQ Sauce",
---   11.99,
---   2200
+--   "McNic",
+--   "A beautiful burger full of love and happiness.",
+--   99.99,
+--   0
 -- );
 
 -- -- GET FROM DB_TABLE
--- SELECT * FROM burgers;
+-- SELECT * FROM noseyjaguar.burgers;
 
 -- -- EDIT RECORD
--- UPDATE burgers SET
+-- UPDATE noseyjaguar.burgers SET
 --   description = "Tasty meat with Pineapple!"
 --   WHERE id = 1;
 
 -- -- REMOVE RECORD
--- DELETE FROM burgers WHERE id = 1;
+-- DELETE FROM noseyjaguar.burgers WHERE id = 1;
 
--- DROP TABLE burgers;
+-- DROP TABLE noseyjaguar.burgers;
 
--- DROP TABLE users;
+-- DROP TABLE noseyjaguar.users;
 
--- CREATE TABLE users (
+-- CREATE TABLE noseyjaguar.users (
 --   id VARCHAR(255) NOT NULL,
 --   name VARCHAR(255),
 --   email VARCHAR(255) NOT NULL,
